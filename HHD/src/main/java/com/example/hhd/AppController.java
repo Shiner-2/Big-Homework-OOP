afef34cf8c626f0469bc34b7fb50ffe938e58109
Shiner-2 <pnhd2005@gmail.com>
2024-03-19 16:08:55 +0700

    first UI
    First UI design

--- HHD/src/main/java/com/example/hhd/AppController.java
+++ HHD/src/main/java/com/example/hhd/AppController.java
@@ -3,7 +3,7 @@ package com.example.hhd;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class AppController {
     @FXML
     private Label welcomeText;
 
