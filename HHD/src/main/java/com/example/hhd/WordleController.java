27c70586defff224435fce13a76a0acdb53271be
Shiner-2 <pnhd2005@gmail.com>
2024-04-16 15:26:55 +0700

    WordleV2
    Almost Done with Wordle

--- HHD/src/main/java/com/example/hhd/WordleController.java
+++ HHD/src/main/java/com/example/hhd/WordleController.java
@@ -3,6 +3,7 @@ package com.example.hhd;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
@@ -11,10 +12,15 @@ import java.util.ResourceBundle;
 
 public class WordleController implements Initializable {
     @FXML
+    private Button btn;
+    @FXML
     private VBox WordleContainer;
     @FXML
     private TextField WordleInput;
     private Integer sz = 5;
+    private String HiddenWord = "DOGGY";
+    private TrieDictionary trie = new TrieDictionary();
+    private Integer GuessCount = 0;
 
     public void onWordleBtnSubmit() {
         String in = WordleInput.getText();
@@ -26,10 +32,30 @@ public class WordleController implements Initializable {
             a.setContentText("Please enter " + sz.toString() + " letters!");
             a.show();
         } else{
-            WordleWordController lt = new WordleWordController(getSz());
-            lt.setWord(in);
-            WordleContainer.getChildren().add(lt);
+            if(!trie.contains(in)) {
+                WordleWordController lt = new WordleWordController(getSz());
+                lt.setWord(in, HiddenWord);
+                WordleContainer.getChildren().add(lt);
+                GuessCount++;
+            } else{
+                Alert a = new Alert(Alert.AlertType.ERROR);
+                a.setTitle("Error");
+                a.setHeaderText("Invalid Word");
+                a.setContentText("Please enter an English word");
+                a.show();
+            }
         }
+
+        if(GuessCount >= 5) {
+            WordleInput.setDisable(true);
+            btn.setDisable(true);
+            Alert a = new Alert(Alert.AlertType.INFORMATION);
+            a.setTitle("You lose");
+            a.setHeaderText("Noob");
+            a.setContentText("Better luck next time");
+            a.show();
+        }
+
     }
 
     public void setSz(Integer sz) {
--- HHD/src/main/java/com/example/hhd/WordleWordController.java
+++ HHD/src/main/java/com/example/hhd/WordleWordController.java
@@ -7,11 +7,13 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class WordleWordController extends VBox {
     @FXML
     private HBox WordleWordContainer;
-    private Integer sz;
+    private Integer sz = 5;
+    private TrieDictionary trie = new TrieDictionary();
 
     public WordleWordController(Integer sz) {
         this.sz = sz;
@@ -28,10 +30,34 @@ public class WordleWordController extends VBox {
 
     }
 
-    public void setWord(String s) {
+    public void setWord(String s, String HiddenWord) {
+        HashMap<Character,Integer> mp = new HashMap<>();
         for(int i = 0 ; i < sz; i++) {
+            if (s.charAt(i) == HiddenWord.charAt(i)) continue;
+            Character c = HiddenWord.charAt(i);
+            if (mp.containsKey(c)) {
+                Integer cur = mp.get(c);
+                mp.put(c,cur+1);
+            } else{
+                mp.put(c,1);
+            }
+        }
+        for(int i = 0 ; i < sz; i++) {
+            char User = s.charAt(i);
+            char Hidden = HiddenWord.charAt(i);
             WordleLetterController lt = new WordleLetterController();
             lt.setLetter(String.valueOf(s.charAt(i)));
+            if(s.charAt(i) == HiddenWord.charAt(i)) {
+                lt.setStyle("-fx-background-color: green;");
+            } else{
+                if (mp.containsKey(User) && mp.get(User) > 0) {
+                    Integer cur = mp.get(User);
+                    mp.put(User,cur-1);
+                    lt.setStyle("-fx-background-color: yellow;");
+                } else {
+                    lt.setStyle("-fx-background-color: red;");
+                }
+            }
             WordleWordContainer.getChildren().add(lt);
         }
     }
