ced9e83e1b39da4c1cc71b1a912031ea2faad53a
Shiner-2 <pnhd2005@gmail.com>
2024-05-08 23:12:21 +0700

    Final 2

--- HHD/src/main/java/com/example/hhd/DictionaryController.java
+++ HHD/src/main/java/com/example/hhd/DictionaryController.java
@@ -198,7 +198,7 @@ public class DictionaryController implements Initializable {
         newWindow.setScene(new Scene(loader.load()));
         newWindow.show();
         newWindow.setOnCloseRequest(windowEvent -> {
-            System.out.println("close");
+            //System.out.println("close");
             try {
                 data = new TrieDictionary();
             } catch (IOException e) {
