90c0d7abd8c60c1cc57b99fdee9ed92ae4a15ca1
hungp2812 <hungp2812@gmail.com>
2024-05-08 14:40:18 +0700

    update (dictionary's delete and insert will write directly to file)

--- HHD/src/test/java/TTSTest.java
+++ HHD/src/test/java/TTSTest.java
@@ -1,64 +1,7 @@
-import javax.speech.Central;
-import javax.speech.synthesis.Synthesizer;
-import javax.speech.synthesis.SynthesizerModeDesc;
-import javax.speech.synthesis.Voice;
-import java.util.Locale;
-
 public class TTSTest {
-    String voiceName = "kevin16";
-    public void speak(String text) {
-        //System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
-        try {
-            SynthesizerModeDesc desc = new SynthesizerModeDesc(
-                    null,          // engine name
-                    "general",     // mode name
-                    Locale.US,     // locale
-                    null,          // running
-                    null);         // voice
-            Synthesizer synthesizer = Central.createSynthesizer(desc);
-
-            /* Get the synthesizer ready to speak
-             */
-            synthesizer.allocate();
-            synthesizer.resume();
-
-            /* Choose the voice.
-             */
-            desc = (SynthesizerModeDesc) synthesizer.getEngineModeDesc();
-            Voice[] voices = desc.getVoices();
-            Voice voice = null;
-            for (int i = 0; i < voices.length; i++) {
-                if (voices[i].getName().equals(voiceName)) {
-                    voice = voices[i];
-                    break;
-                }
-            }
-            synthesizer.getSynthesizerProperties().setVoice(voice);
-
-            /* The the synthesizer to speak and wait for it to
-             * complete.
-             */
-            synthesizer.speakPlainText(text, null);
-            synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
-
-            /* Clean up and leave.
-             */
-            // synthesizer.deallocate();
-//            System.exit(0);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public TTSTest() {
-        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
-    }
-
     public static void main(String[] args) {
-        TTSTest test = new TTSTest();
-        test.speak("Hello");
+//        TTSTest test = new TTSTest();
+//        test.speak("Hello");
         System.exit(0);
     }
 }
